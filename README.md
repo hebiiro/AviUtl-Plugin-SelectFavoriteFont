66b84f984104bb17b2537c5bffa5f2f86e25c17a
hebiiro <96464759+hebiiro@users.noreply.github.com>
2021-12-25 12:06:07 +0900

    1.1.0
    爆速プラグインと共存できるように修正

--- README.md
+++ README.md
@@ -1,4 +1,36 @@
-# AviUtlプラグイン - お気に入りフォント選択
-ビルドして得られる SelectFavoriteFont.auf は AviUtl のプラグインです。  
-AviUtl からプラグインとして読み込まれると動作します。単体では何もできません。  
-拡張編集プラグインに作用するので拡張編集を入れていないと意味がないです。  
+# AviUtl プラグイン - お気に入りフォント選択
+
+* version 1.1.0 by 蛇色 - 2021/12/25 爆速プラグインと共存できるように修正
+* version 1.0.0 by 蛇色 - 2021/12/22 初版
+
+テキストオブジェクトのオブジェクト編集ダイアログで
+お気に入り、またはよく使うフォントからフォントを選択できるようにします。
+
+## インストール
+
+以下のファイルを AviUtl の Plugins フォルダに入れてください。
+* SelectFavoriteFont.auf
+* SelectFavoriteFont.popular.txt
+* SelectFavoriteFont.favorite.txt
+
+## 使い方
+
+1. まずテキストオブジェクトのオブジェクト編集ダイアログで普通にフォントを選択します。
+2. 段々その横のコンボボックスにフォントが溜まっていくので、ある程度溜まったら一旦 AviUtl を終了します。
+3. SelectFavoriteFont.popular.txt をテキストエディタで開き、お気に入りのフォントをカットして取り除きます。
+4. SelectFavoriteFont.favorite.txt をテキストエディタで開き、お気に入りのフォントをペーストして追加します。
+5. 1～4 を繰り返してお気に入りのフォントを構築します。
+6. フォント選択コンボボックスの左に生えているコンボボックスが、よく使うフォント、その下がお気に入りフォントになっているので、これらのコンボボックスを使ってフォントを選択します。
+
+※「キャンセル」を押しても最後に選択したフォルダが残るのは仕様です。
+
+## 設定
+
+現時点では設定で変更できる所はありません。
+
+## 動作確認
+
+* (必須) AviUtl 1.10 & 拡張編集 0.92 http://spring-fragrance.mints.ne.jp/aviutl/
+* (共存確認) patch.aul r8 https://scrapbox.io/ePi5131/patch.aul
+* (共存確認) eclipse_fast 1.00 https://www.nicovideo.jp/watch/sm39756003
+* (共存確認) 爆速プラグイン 1.06 https://www.nicovideo.jp/watch/sm39679253
--- SelectFavoriteFont/SelectFavoriteFont.cpp
+++ SelectFavoriteFont/SelectFavoriteFont.cpp
@@ -6,7 +6,7 @@
 EXTERN_C FILTER_DLL __declspec(dllexport) * __stdcall GetFilterTable(void)
 {
 	static TCHAR g_filterName[] = TEXT("お気に入りフォント選択");
-	static TCHAR g_filterInformation[] = TEXT("お気に入りフォント選択 version 1.0.0 by 蛇色");
+	static TCHAR g_filterInformation[] = TEXT("お気に入りフォント選択 version 1.1.0 by 蛇色");
 
 	static FILTER_DLL g_filter =
 	{
@@ -39,25 +39,25 @@ EXTERN_C FILTER_DLL __declspec(dllexport) * __stdcall GetFilterTable(void)
 
 //---------------------------------------------------------------------
 
-const UINT WM_HEBIIRO_SUBCLASS_FONT_COMBOBOX = ::RegisterWindowMessage(_T("WM_HEBIIRO_SUBCLASS_FONT_COMBOBOX"));
 const UINT ID_FONT_COMBO_BOX = 0x5654;
 
 HINSTANCE g_instance = 0; // この DLL のハンドル
+HFONT g_font = 0; // コントロール用のフォントハンドル
 TCHAR g_popularFileName[MAX_PATH] = {};
 TCHAR g_favoriteFileName[MAX_PATH] = {};
+BOOL g_defaultProcessingOnly = FALSE;
 
-HWND g_exeditObjectDialog = 0;
-HWND g_fontComboBox = 0;
 HHOOK g_hook = 0;
-BOOL g_noNotify = FALSE;
-
+HWND g_exeditObjectDialog = 0;
+HWND g_container = 0;
 HWND g_popular = 0;
 HWND g_favorite = 0;
 
-WNDPROC orig_popular_wndProc = 0;
-WNDPROC orig_favorite_wndProc = 0;
-WNDPROC orig_fontComboBox_wndProc = 0;
-WNDPROC orig_exeditObjectDialog_wndProc = 0;
+//---------------------------------------------------------------------
+
+int GetWidth(LPCRECT rc);
+int GetHeight(LPCRECT rc);
+void SetClientRect(HWND hwnd, int x, int y, int w, int h);
 
 void readFile(HWND comboBox, LPCTSTR fileName);
 void writeFile(HWND comboBox, LPCTSTR fileName);
@@ -65,30 +65,72 @@ int findString(HWND comboBox, LPCTSTR fontName);
 void addFontName(HWND comboBox, LPCTSTR fontName);
 int selectFontName(HWND comboBox, LPCTSTR fontName);
 tstring getCurrentText(HWND comboBox);
-void showControls(BOOL visible);
-HWND createMyFontComboBox();
-void subclassFontComboBox(HWND fontComboBox);
 
-LRESULT CALLBACK cbtProc(int code, WPARAM wParam, LPARAM lParam);
+HWND createMyFontComboBox(HWND parent, LPCTSTR windowName);
+void createContainer(HWND exeditObjectDialog);
+void destroyContainer();
+void showContainer();
+void hideContainer();
+
+LRESULT CALLBACK container_wndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK hook_exeditObjectDialog_wndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK cwprProc(int code, WPARAM wParam, LPARAM lParam);
 void hook();
 void unhook();
 
-LRESULT CALLBACK hook_popular_wndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
-LRESULT CALLBACK hook_favorite_wndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
-LRESULT CALLBACK hook_fontComboBox_wndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
-LRESULT CALLBACK hook_exeditObjectDialog_wndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+//---------------------------------------------------------------------
+
+// 矩形の幅を返す
+inline int GetWidth(LPCRECT rc)
+{
+	return rc->right - rc->left;
+}
+
+// 矩形の高さを返す
+inline int GetHeight(LPCRECT rc)
+{
+	return rc->bottom - rc->top;
+}
+
+// クライアント領域が指定された位置に来るようにウィンドウの位置を調整する
+void SetClientRect(HWND hwnd, int x, int y, int w, int h)
+{
+	RECT rcWindow; ::GetWindowRect(hwnd, &rcWindow);
+	RECT rcClient; ::GetClientRect(hwnd, &rcClient);
+//	::MapWindowPoints(hwnd, NULL, (LPPOINT)&rcClient, 2); // 幅と高さしか使わないので座標系を合わせる必要はない
+	int rcWindowWidth = GetWidth(&rcWindow);
+	int rcWindowHeight = GetHeight(&rcWindow);
+	int rcClientWidth = GetWidth(&rcClient);
+	int rcClientHeight = GetHeight(&rcClient);
+	POINT diff =
+	{
+		rcWindowWidth - rcClientWidth,
+		rcWindowHeight - rcClientHeight,
+	};
+
+	x -= diff.x / 2;
+	y -= diff.y / 2;
+	w += diff.x;
+	h += diff.y;
+
+	::SetWindowPos(hwnd, NULL,
+		x, y, w, h, SWP_NOZORDER | SWP_NOACTIVATE);
+}
 
 //---------------------------------------------------------------------
 
+// フォントリストをファイルから読み込む
 void readFile(HWND comboBox, LPCTSTR fileName)
 {
 	_RPTFTN(_T("readFile(0x%08X, %s)\n"), comboBox, fileName);
+
+	ComboBox_ResetContent(comboBox);
 
 	// 入力ストリームを開く。
 	std::ifstream file(fileName);
 
 	tstring line;
-	while (std::getline(file, line)) // 1行ずつ読み込む。
+	while (std::getline(file, line)) // 1行ずつ読み込む
 	{
 		// コンボボックスに読み込んだテキストを追加する。
 		ComboBox_AddString(comboBox, line.c_str());
@@ -98,6 +140,7 @@ void readFile(HWND comboBox, LPCTSTR fileName)
 	file.close();
 }
 
+// フォントリストをファイルに書き込む
 void writeFile(HWND comboBox, LPCTSTR fileName)
 {
 	_RPTFTN(_T("writeFile(0x%08X, %s)\n"), comboBox, fileName);
@@ -128,7 +171,7 @@ void writeFile(HWND comboBox, LPCTSTR fileName)
 	file.close();
 }
 
-// コンボボックスのリストから fontName を見つけてそのインデックスを返す。
+// コンボボックスのリストから fontName を見つけてそのインデックスを返す
 // (CB_FINDSTRING は部分一致でもヒットするので使用できない)
 int findString(HWND comboBox, LPCTSTR fontName)
 {
@@ -148,6 +191,7 @@ int findString(HWND comboBox, LPCTSTR fontName)
 		_RPTF_STR(text.c_str());
 		if (text.empty()) continue;
 
+		// テキストが完全一致するかチェックする。
 		if (::lstrcmp(text.c_str(), fontName) == 0)
 			return i;
 	}
@@ -155,6 +199,7 @@ int findString(HWND comboBox, LPCTSTR fontName)
 	return CB_ERR;
 }
 
+// コンボボックスにテキストを追加する。ただし、すでに存在する場合は追加しない
 void addFontName(HWND comboBox, LPCTSTR fontName)
 {
 	// 指定されたフォント名がすでに存在するか調べる。
@@ -170,6 +215,7 @@ void addFontName(HWND comboBox, LPCTSTR fontName)
 	ComboBox_SetCurSel(comboBox, 0);
 }
 
+// 指定されたテキストを持つアイテムを選択肢し、そのインデックスを返す
 int selectFontName(HWND comboBox, LPCTSTR fontName)
 {
 	// 指定されたフォント名が存在するか調べる。
@@ -184,6 +230,7 @@ int selectFontName(HWND comboBox, LPCTSTR fontName)
 	return index;
 }
 
+// コンボボックスで現在選択されているアイテムのテキストを返す
 tstring getCurrentText(HWND comboBox)
 {
 	// 選択インデックスを取得する。
@@ -205,204 +252,229 @@ tstring getCurrentText(HWND comboBox)
 	return text;
 }
 
-// 自作コントロールの位置を調整し、表示する。
-void showControls(BOOL visible)
-{
-	if (visible)
-	{
-		RECT rc; ::GetWindowRect(g_fontComboBox, &rc);
+//---------------------------------------------------------------------
 
-		int w = (rc.right - rc.left);
-		int h = (rc.bottom - rc.top);
-		int w2 = w * 2;
-		int h2 = h * 24;
-
-		{
-			int x = rc.left - w;
-			int y = rc.top;
-
-			::SendMessage(g_popular, CB_SETDROPPEDWIDTH, w2, 0);
-			::SetWindowPos(g_popular, 0, x, y, w, h2,
-				SWP_NOZORDER | SWP_NOACTIVATE | SWP_SHOWWINDOW);
-		}
-
-		{
-			int x = rc.left - w;
-			int y = rc.top + h;
-
-			::SendMessage(g_favorite, CB_SETDROPPEDWIDTH, w2, 0);
-			::SetWindowPos(g_favorite, 0, x, y, w, h2,
-				SWP_NOZORDER | SWP_NOACTIVATE | SWP_SHOWWINDOW);
-		}
-	}
-	else
-	{
-		::ShowWindow(g_popular, SW_HIDE);
-		::ShowWindow(g_favorite, SW_HIDE);
-	}
-}
-
-// コンボボックスを作成する。
-HWND createMyFontComboBox()
+// フォントコンボボックスを作成する
+HWND createMyFontComboBox(HWND parent, LPCTSTR windowName)
 {
 	HWND hwnd = ::CreateWindowEx(
 		WS_EX_NOACTIVATE,
 		_T("ComboBox"),
-		_T("SelectFavoriteFont"),
-		WS_POPUP | WS_HSCROLL | WS_VSCROLL |
+		windowName,
+		WS_CHILD | WS_VISIBLE |
+		WS_HSCROLL | WS_VSCROLL |
 //		CBS_AUTOHSCROLL | CBS_DISABLENOSCROLL |
 		CBS_DROPDOWNLIST | CBS_HASSTRINGS,
 		0, 0, 100, 100,
-		g_exeditObjectDialog,
+		parent,
 		0,
 		g_instance,
 		0);
 	_RPTF_HEX(hwnd);
 
-	// フォントを元のコンボボックスのものに合わせる。
-	HFONT font = GetWindowFont(g_fontComboBox);
-	_RPTF_HEX(font);
-	SetWindowFont(hwnd, font, TRUE);
+	// ここでフォントもセットする。
+	SetWindowFont(hwnd, g_font, TRUE);
 
 	return hwnd;
 }
 
-// exedit のフォントコンボボックスをサブクラス化する。
-void subclassFontComboBox(HWND fontComboBox)
+// コンテナウィンドウとコントロール群を作成し、初期設定する
+void createContainer(HWND exeditObjectDialog)
 {
-	_RPTFT0(_T("subclassFontComboBox()\n"));
+	_RPTFTN(_T("createContainer(0x%08X)\n"), exeditObjectDialog);
 
-	// フォントコンボボックスをサブクラス化する。
-	g_fontComboBox = fontComboBox;
-	_RPTF_HEX(g_fontComboBox);
-	orig_fontComboBox_wndProc = SubclassWindow(g_fontComboBox, hook_fontComboBox_wndProc);
-	_RPTF_HEX(orig_fontComboBox_wndProc);
+	// オブジェクト編集ダイアログのウィンドウハンドルをグローバル変数に保存しておく。
+	g_exeditObjectDialog = exeditObjectDialog;
+	_RPTF_HEX(g_exeditObjectDialog);
+
+	// コンテナウィンドウのウィンドウクラスを登録する。
+	WNDCLASSEX wc = {};
+	wc.cbSize = sizeof(wc);
+	wc.lpszClassName = _T("SelectFavoriteFont");
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = container_wndProc;
+	wc.hInstance = g_instance;
+	wc.hCursor = ::LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW  + 1);
+	::RegisterClassEx(&wc);
+
+	// コンテナウィンドウを作成する。
+	g_container = ::CreateWindowEx(
+		WS_EX_NOACTIVATE,
+		_T("SelectFavoriteFont"),
+		_T("SelectFavoriteFont"),
+		WS_CLIPCHILDREN | WS_CLIPSIBLINGS |
+		WS_POPUP,
+//		WS_POPUP | WS_BORDER, // コンテナウィンドウに細い枠を付けたいならこっちを使う
+//		WS_POPUP | WS_DLGFRAME, // コンテナウィンドウに立体枠を付けたいならこっちを使う
+		0, 0, 100, 100,
+		g_exeditObjectDialog,
+		0,
+		g_instance,
+		0);
+	_RPTF_HEX(g_container);
 
 	// popular コンボボックスを作成し、初期設定する。
-	g_popular = createMyFontComboBox();
+	g_popular = createMyFontComboBox(g_container, _T("popular"));
 	_RPTF_HEX(g_popular);
-	orig_popular_wndProc = SubclassWindow(g_popular, hook_popular_wndProc);
-	readFile(g_popular, g_popularFileName);
 
 	// favorite コンボボックスを作成し、初期設定する。
-	g_favorite = createMyFontComboBox();
+	g_favorite = createMyFontComboBox(g_container, _T("favorite"));
 	_RPTF_HEX(g_favorite);
-	orig_favorite_wndProc = SubclassWindow(g_favorite, hook_favorite_wndProc);
-	readFile(g_favorite, g_favoriteFileName);
+}
 
-	// 作成したコントロールを表示する。
-	showControls(::IsWindowVisible(g_fontComboBox));
+// コンテナウィンドウを削除する
+void destroyContainer()
+{
+	_RPTFT0(_T("destroyContainer()\n"));
+
+	::DestroyWindow(g_container);
+}
+
+// コンテナウィンドウを表示する
+void showContainer()
+{
+	_RPTFT0(_T("showContainer()\n"));
+
+	::ShowWindow(g_container, SW_SHOWNA);
+}
+
+// コンテナウィンドウを非表示にする
+void hideContainer()
+{
+	_RPTFT0(_T("hideContainer()\n"));
+
+	::ShowWindow(g_container, SW_HIDE);
+}
+
+// コンテナウィンドウとコントロール群の表示位置を調整する
+void moveContainer()
+{
+	_RPTFT0(_T("moveContainer()\n"));
+
+	// 基準となる AviUtl (exedit) 側のコントロールを取得する。
+	HWND base = ::GetDlgItem(g_exeditObjectDialog, ID_FONT_COMBO_BOX);
+	if (!base) return;
+	if (!::IsWindowVisible(base)) return;
+	RECT rcBase; ::GetWindowRect(base, &rcBase);
+	int rcBaseWidth = GetWidth(&rcBase);
+	int rcBaseHeight = GetHeight(&rcBase);
+
+	{
+		int x = rcBase.left - rcBaseWidth;
+		int y = rcBase.top - rcBaseHeight;
+		int w = rcBaseWidth;
+		int h = rcBaseHeight * 2;
+
+		SetClientRect(g_container, x, y, w, h);
+	}
+
+	HDWP dwp = ::BeginDeferWindowPos(2); // 引数は動かすウィンドウの数
+	_RPTF_HEX(dwp);
+	if (!dwp) return;
+
+	{
+		// 「よく使う」フォントコンボボックスを動かす。
+
+		int x = 0;
+		int y = 0;
+		int w = rcBaseWidth;
+		int h = rcBaseHeight;
+		int w2 = w * 2;
+		int h2 = h * 24;
+
+		::SendMessage(g_popular, CB_SETDROPPEDWIDTH, w2, 0);
+		dwp = ::DeferWindowPos(dwp, g_popular, NULL,
+			x, y, w, h2, SWP_NOZORDER | SWP_NOACTIVATE);
+		_RPTF_HEX(dwp);
+		if (!dwp) return;
+	}
+
+	{
+		// 「お気に入り」フォントコンボボックスを動かす。
+
+		int x = 0;
+		int y = rcBaseHeight;
+		int w = rcBaseWidth;
+		int h = rcBaseHeight;
+		int w2 = w * 2;
+		int h2 = h * 24;
+
+		::SendMessage(g_favorite, CB_SETDROPPEDWIDTH, w2, 0);
+		dwp = ::DeferWindowPos(dwp, g_favorite, NULL,
+			x, y, w, h2, SWP_NOZORDER | SWP_NOACTIVATE);
+		_RPTF_HEX(dwp);
+		if (!dwp) return;
+	}
+
+	BOOL result = ::EndDeferWindowPos(dwp);
+	_RPTF_NUM(result);
 }
 
 //---------------------------------------------------------------------
 
-LRESULT CALLBACK hook_popular_wndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK container_wndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	switch (message)
 	{
 	case WM_COMMAND:
 		{
-			_RPTFTN(_T("hook_popular_wndProc(WM_COMMAND, 0x%08X, 0x%08X)\n"), wParam, lParam);
+			_RPTFTN(_T("container_wndProc(WM_COMMAND, 0x%08X, 0x%08X)\n"), wParam, lParam);
 
+			// WM_COMMAND メッセージの引数を分解する。
 			UINT code = HIWORD(wParam);
 			UINT id = LOWORD(wParam);
 			HWND sender = (HWND)lParam;
 
-			if (code == LBN_SELCHANGE)
+			if (code == CBN_SELCHANGE) // コンテナ内のコンボボックスの選択が切り替えられた
 			{
-				_RPTFTN(_T("hook_popular_wndProc(LBN_SELCHANGE, 0x%08X, 0x%08X)\n"), id, sender);
+				_RPTFTN(_T("container_wndProc(CBN_SELCHANGE, 0x%08X, 0x%08X)\n"), id, sender);
 
-				tstring text = getCurrentText(hwnd);
-				ComboBox_SelectString(g_fontComboBox, 0, text.c_str());
-				g_noNotify = TRUE;
-				::SendMessage(g_exeditObjectDialog, WM_COMMAND,
-					MAKEWPARAM(ID_FONT_COMBO_BOX, CBN_SELCHANGE), (LPARAM)g_fontComboBox);
-				g_noNotify = FALSE;
+				// フォントコンボボックスを取得する。
+				HWND fontComboBox = ::GetDlgItem(g_exeditObjectDialog, ID_FONT_COMBO_BOX);
+				if (!fontComboBox) break;
+
+				// 選択されているフォント名を取得する。
+				tstring text = getCurrentText(sender);
+
+				// フォントコンボボックスの選択を切り替える。
+				ComboBox_SelectString(fontComboBox, 0, text.c_str());
+
+				// デフォルト処理のみ実行する。
+				g_defaultProcessingOnly = TRUE;
+				::SendMessage(
+					g_exeditObjectDialog,
+					WM_COMMAND,
+					MAKEWPARAM(ID_FONT_COMBO_BOX, CBN_SELCHANGE),
+					(LPARAM)fontComboBox);
+				g_defaultProcessingOnly = FALSE;
 			}
 
 			break;
 		}
 	case WM_DESTROY:
 		{
-			_RPTFTN(_T("hook_popular_wndProc(WM_DESTROY, 0x%08X, 0x%08X)\n"), wParam, lParam);
-
-			writeFile(hwnd, g_popularFileName);
+			_RPTFTN(_T("WM_DESTROY, 0x%08X, 0x%08X\n"), wParam, lParam);
 
 			break;
 		}
 	}
 
-	return ::CallWindowProc(orig_fontComboBox_wndProc, hwnd, message, wParam, lParam);
-}
-
-LRESULT CALLBACK hook_favorite_wndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-	switch (message)
-	{
-	case WM_COMMAND:
-		{
-			_RPTFTN(_T("hook_favorite_wndProc(WM_COMMAND, 0x%08X, 0x%08X)\n"), wParam, lParam);
-
-			UINT code = HIWORD(wParam);
-			UINT id = LOWORD(wParam);
-			HWND sender = (HWND)lParam;
-
-			if (code == LBN_SELCHANGE)
-			{
-				_RPTFTN(_T("hook_favorite_wndProc(LBN_SELCHANGE, 0x%08X, 0x%08X)\n"), id, sender);
-
-				tstring text = getCurrentText(hwnd);
-				ComboBox_SelectString(g_fontComboBox, 0, text.c_str());
-				g_noNotify = TRUE;
-				::SendMessage(g_exeditObjectDialog, WM_COMMAND,
-					MAKEWPARAM(ID_FONT_COMBO_BOX, CBN_SELCHANGE), (LPARAM)g_fontComboBox);
-				g_noNotify = FALSE;
-			}
-
-			break;
-		}
-	}
-
-	return ::CallWindowProc(orig_fontComboBox_wndProc, hwnd, message, wParam, lParam);
-}
-
-LRESULT CALLBACK hook_fontComboBox_wndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-	switch (message)
-	{
-	case WM_SHOWWINDOW:
-		{
-			_RPTFTN(_T("hook_fontComboBox_wndProc(WM_SHOWWINDOW, %d)\n"), wParam);
-
-			showControls((BOOL)wParam);
-
-			break;
-		}
-	}
-
-	return ::CallWindowProc(orig_fontComboBox_wndProc, hwnd, message, wParam, lParam);
+	return ::DefWindowProc(hwnd, message, wParam, lParam);
 }
 
 LRESULT CALLBACK hook_exeditObjectDialog_wndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	switch (message)
 	{
-	case WM_PARENTNOTIFY:
-		{
-//			_RPTFTN(_T("hook_exeditObjectDialog_wndProc(WM_PARENTNOTIFY, 0x%08X, 0x%08X)\n"), wParam, lParam);
-
-			if (wParam == WM_CREATE)
-				::PostMessage(hwnd, WM_HEBIIRO_SUBCLASS_FONT_COMBOBOX, wParam, lParam);
-
-			break;
-		}
 	case WM_COMMAND:
 		{
 //			_RPTFTN(_T("hook_exeditObjectDialog_wndProc(WM_COMMAND, 0x%08X, 0x%08X)\n"), wParam, lParam);
 
-			if (g_noNotify) // フラグが立っている場合は通知を処理しない。
-				break;
+			if (g_defaultProcessingOnly)
+				break; // フラグが立っている場合は通常処理だけ。独自処理は行わない
 
+			// WM_COMMAND メッセージの引数を分解する。
 			UINT code = HIWORD(wParam);
 			UINT id = LOWORD(wParam);
 			HWND sender = (HWND)lParam;
@@ -425,60 +497,104 @@ LRESULT CALLBACK hook_exeditObjectDialog_wndProc(HWND hwnd, UINT message, WPARAM
 
 			break;
 		}
-	}
-
-	if (message == WM_HEBIIRO_SUBCLASS_FONT_COMBOBOX)
-	{
-		HWND hwnd = (HWND)lParam;
-		UINT id = ::GetDlgCtrlID(hwnd);
-//		_RPTFTN(_T("hook_exeditObjectDialog_wndProc(WM_HEBIIRO_SUBCLASS_FONT_COMBOBOX, 0x%08X, 0x%08X)\n"), hwnd, id);
-		if (id == ID_FONT_COMBO_BOX)
+#if 1
+	case WM_WINDOWPOSCHANGED:
 		{
-			subclassFontComboBox(hwnd);
+			_RPTFTN(_T("WM_WINDOWPOSCHANGED, 0x%08X, 0x%08X\n"), wParam, lParam);
+
+			// コンテナウィンドウをターゲットダイアログに追従させる。
+			moveContainer();
+
+			break;
 		}
+#endif
 	}
 
-	return ::CallWindowProc(orig_exeditObjectDialog_wndProc, hwnd, message, wParam, lParam);
+	return 0;
 }
 
-//---------------------------------------------------------------------
-
-LRESULT CALLBACK cbtProc(int code, WPARAM wParam, LPARAM lParam)
+// CallWndProcRet フック関数
+LRESULT CALLBACK cwprProc(int code, WPARAM wParam, LPARAM lParam)
 {
 	if (code >= 0)
 	{
-		if (code == HCBT_CREATEWND)
+		CWPRETSTRUCT* cwpr = (CWPRETSTRUCT*)lParam;
+
+		switch(cwpr->message)
 		{
-			CBT_CREATEWND* cw = (CBT_CREATEWND*)lParam;
-
-			if ((DWORD)cw->lpcs->lpszClass > 0xFFFF &&
-				::lstrcmpi(cw->lpcs->lpszClass, _T("ExtendedFilterClass")) == 0)
+		case WM_CREATE:
 			{
-				// exedit のオブジェクト編集ダイアログをサブクラス化する。
-				g_exeditObjectDialog = (HWND)wParam;
-				_RPTF_HEX(g_exeditObjectDialog);
-				orig_exeditObjectDialog_wndProc = SubclassWindow(
-					g_exeditObjectDialog, hook_exeditObjectDialog_wndProc);
-				_RPTF_HEX(orig_exeditObjectDialog_wndProc);
+//				_RPTFT0(_T("WM_CREATE\n"));
 
-				// フックはこれで終わり。
-				unhook();
+				// このウィンドウのクラス名を取得する。
+				TCHAR className[MAX_PATH] = {};
+				::GetClassName(cwpr->hwnd, className, MAX_PATH);
+//				_RPTF_STR(className);
+
+				if (::StrStr(className, _T("ExtendedFilterClass")))
+				{
+					_RPTFT0(_T("拡張編集のオブジェクト編集ダイアログをフックします\n"));
+
+					createContainer(cwpr->hwnd);
+				}
+
+				break;
 			}
+		case WM_SHOWWINDOW:
+			{
+//				_RPTFTN(_T("WM_SHOWWINDOW, 0x%08X, 0x%08X\n"), cwpr->wParam, cwpr->lParam);
+
+				// フォントコンボボックスのメッセージかチェックする。
+				HWND control = cwpr->hwnd;
+				UINT id = ::GetDlgCtrlID(control);
+//				_RPTF_NUM(id);
+
+				if (id == ID_FONT_COMBO_BOX)
+				{
+					_RPTFT0(_T("フォントコンボボックスの WM_SHOWWINDOW\n"));
+
+					if (cwpr->wParam)
+					{
+						_RPTFT0(_T("フォントコンボボックスが表示された\n"));
+
+						readFile(g_popular, g_popularFileName);
+						readFile(g_favorite, g_favoriteFileName);
+
+						moveContainer();
+						showContainer();
+					}
+					else
+					{
+						_RPTFT0(_T("フォントコンボボックスが非表示になった\n"));
+
+						hideContainer();
+
+						writeFile(g_popular, g_popularFileName);
+					}
+				}
+
+				break;
+			}
+		}
+
+		if (cwpr->hwnd == g_exeditObjectDialog)
+		{
+			hook_exeditObjectDialog_wndProc(cwpr->hwnd, cwpr->message, cwpr->wParam, cwpr->lParam);
 		}
 	}
 
 	return ::CallNextHookEx(g_hook, code, wParam, lParam);
 }
 
-// ファイル選択ダイアログのフックを開始する
+// CallWndProcRet フックを作成する
 void hook()
 {
 	_RPTFT0(_T("hook()\n"));
 
-	g_hook = ::SetWindowsHookEx(WH_CBT, cbtProc, 0, ::GetCurrentThreadId());
+	g_hook = ::SetWindowsHookEx(WH_CALLWNDPROCRET, cwprProc, 0, ::GetCurrentThreadId());
 }
 
-// ファイル選択ダイアログのフックを終了する
+// CallWndProcRet フックを削除する
 void unhook()
 {
 	_RPTFT0(_T("unhook()\n"));
@@ -492,16 +608,29 @@ void unhook()
 
 BOOL func_init(FILTER *fp)
 {
+	// ロケールを設定する。これをやらないと日本語テキストが文字化けする。
 	_tsetlocale(LC_ALL, _T(""));
 
 	_RPTFT0(_T("func_init()\n"));
 
+	// 色んな所で使う可能性があるので、
+	// この DLL のハンドルをグローバル変数に保存しておく。
 	g_instance = fp->dll_hinst;
+	_RPTF_HEX(g_instance);
+
+	// フォントリストファイルのパスを取得しておく。
 	::GetModuleFileName(g_instance, g_popularFileName, MAX_PATH);
 	::PathRenameExtension(g_popularFileName, _T(".popular.txt"));
 	::GetModuleFileName(g_instance, g_favoriteFileName, MAX_PATH);
 	::PathRenameExtension(g_favoriteFileName, _T(".favorite.txt"));
 
+	// コントロール用のフォントを取得しておく。
+	SYS_INFO si = {};
+	fp->exfunc->get_sys_info(NULL, &si);
+	g_font = si.hfont;
+	_RPTF_HEX(g_font);
+
+	// CallWndProcRet フックを開始する。
 	hook();
 
 	return TRUE;
@@ -514,6 +643,7 @@ BOOL func_exit(FILTER *fp)
 {
 	_RPTFT0(_T("func_term()\n"));
 
+	// CallWndProcRet フックを終了する。
 	unhook();
 
 	return TRUE;
